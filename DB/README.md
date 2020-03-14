53dc35889d0816ff637fa96b0089f1529cf5e6e1
Matthew Jenkins <matthew.c.jenkins@hotmail.com>
2020-03-14 17:26:04 -0600

    Add basic webpage, update readme, add docker compose file

---
+++ DB/README.md
@@ -0,0 +1,2 @@
+# Matt's Budget App
+This is a project by Matt Jenkins to learn more about SQL and web development/php.
---
+++ php/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Budget App</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <h1>My Budget App</h1>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
